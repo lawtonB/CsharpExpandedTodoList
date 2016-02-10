68cf751418c995e0cac22f0de7e90818bee9fb76
Simon Temple Timothy Durward <simonctemple@live.com | timothy.a.durward@gmail.com>
2016-02-10 11:17:21 -0800

    hell yeah

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,32 @@
+using Nancy;
+using System.Collections.Generic;
+using Todo.Objects;
+
+namespace ToDoList
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Get["/tasks"] = _ => {
+        List<Task> allTasks = Task.GetAll();
+        return View["tasks.cshtml", allTasks];
+      };
+      Get["/tasks/new"] = _ => {
+        return View["task_form.cshtml"];
+      };
+      Post["/tasks"] = _ => {
+        Task newTask = new Task(Request.Form["new-task"]);
+        List<Task> allTasks = Task.GetAll();
+        return View["tasks.cshtml", allTasks];
+      };
+      Get["/tasks/{id}"] = parameters => {
+        Task task = Task.Find(parameters.id);
+        return View["/task.cshtml", task];
+      };
+    }
+  }
+}
